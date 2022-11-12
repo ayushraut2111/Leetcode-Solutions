c5a71a8438a2054e68d81ca08bb196b35666a569
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-11-13 02:21:53 +0530

    russian doll envelope

---
+++ 3 some.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        st={}
+        for i in range(len(nums)-1):
+            for j in range(len(nums)):
+                if (nums[i]+nums[j]) not in st:
+                    st[nums[i]+nums[j]]=[[nums[i],nums[j]]]
+                else:
+                    st[nums[i]+nums[j]].append([nums[i],nums[j]])
+        ans=[]
+        for i in nums:
+            if -i in st:
+                x=[i]
+                x.append(st[-i])
+                ans.append(x)
+        print(ans)
+
+s=Solution()
+nums=list(map(int,input().split()))
+print(s.threeSum(nums))
+        
