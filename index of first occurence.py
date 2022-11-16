e29a47aef3e1fa1c64b3b7300803fb71597a6a7b
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-11-16 15:51:46 +0530

    frst occurence

---
+++ index of first occurence.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle not in haystack:
+            return -1
+        else:
+            return haystack.index(needle)
