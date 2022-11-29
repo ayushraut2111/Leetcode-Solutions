17a072a095cccbb46f958ada72b42cd477c4806e
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-11-29 22:54:06 +0530

    remove from the last of a linked list

---
+++ remoove from last of a linked list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# https://leetcode.com/problems/remove-nth-node-from-end-of-list
+class Solution:
+    def removeNthFromEnd(self, he, n: int):
+        temp=head
+        count=0
+        while temp!=None:
+            temp=temp.next
+            count+=1
+        x=count
+        count-=n
+        if count==0:
+            head=head.next
+            return head
+        temp=head
+        curr=temp
+        for i in range(count):
+            curr=temp
+            temp=temp.next
+        if temp.next is not None:
+            curr.next=temp.next
+        else:
+            curr.next=None
+        return head
