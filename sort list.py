34f4f15faf77ce07d07c6d91975852e5a28de9cc
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-12-06 16:32:20 +0530

    ans

---
+++ sort list.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/sort-list/
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def __init__(self):
+        self.xy=None
+    def insertnode(self,i,ptr):
+        t=ListNode(i)
+        if self.xy==None:
+            self.xy=t
+            return self.xy
+        ptr.next=t
+        return ptr.next
+    def sortList(self, head) ->ListNode:
+        temp=head
+        a=[]
+        while temp is not None:
+            a.append(temp.val)
+            temp=temp.next
+        ptr=None
+        a.sort()
+        for i in a:
+           ptr=self.insertnode(i,ptr)
+        return self.xy
