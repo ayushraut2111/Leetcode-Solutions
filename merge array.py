6c69832bdccfff3927c1e203b282e66cb9b910bd
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-12-06 15:56:06 +0530

    merge array

---
+++ merge array.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/merge-sorted-array/
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m,m+n):
+            nums1[i]=nums2[i-m]
+        nums1.sort()
+        
