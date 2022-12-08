278a1c265e759abdf698be820ba85a1aedd7abaa
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-12-09 00:58:44 +0530

    reverse

--- reverse word in string.py
+++ reverse word in string.py
@@ -1,15 +1,18 @@
 # https://leetcode.com/problems/reverse-words-in-a-string/
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s=s.strip()
-        a=s.split(" ")
-        a=a[::-1]
-        while '' in a:
-            a.remove('')
-        print(a)
+        a=[]
+        p=""
+        for i in s:
+            if i!=' ':
+                p+=i
+            elif len(p)!=0 and i==' ':
+                a.append(p)
+                p=''
+        if p!='':
+            a.append(p)
         ans=""
-        for i in a:
+        for i in a[::-1]:
             ans+=i
             ans+=" "
-        ans=ans.strip()
-        return ans
+        return ans[:-1]
