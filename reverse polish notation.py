f29b2df1f25074da4c55f1173f6c1626a4060b96
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2022-12-20 14:55:12 +0530

    mode in bst

---
+++ reverse polish notation.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/evaluate-reverse-polish-notation/
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+        stack=[]
+        for i in tokens:
+            if i=='+' or i=='-' or i=='*' or i=='/':
+                # print(stack)
+                a=stack.pop()
+                b=stack.pop()
+                if i=='+':
+                    stack.append(a+b)
+                elif i=='-':
+                    stack.append(b-a)
+                elif i=='*':
+                    stack.append(a*b)
+                else:
+                    stack.append(int(b/a))
+            else:
+                stack.append(int(i))
+        return stack[0]
