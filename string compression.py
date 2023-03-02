2a3179675c44c4bad7e0026fbc5b47205717c933
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-03-02 15:41:41 +0530

    leetcode

--- string compression.py
+++ string compression.py
@@ -1,12 +1,6 @@
 # https://leetcode.com/problems/string-compression/
 class Solution:
     def compress(self, chars: list[str]) -> int:
-        mp={}
-        for i in chars:
-            if i not in mp:
-                mp[i]=1
-            else:
-                mp[i]+=1
         count=1
         ap=[]
         i=0
@@ -19,26 +13,19 @@ class Solution:
                 if count>1 and count<10:
                     ap.append(str(count))
                 elif count>=10:
-                    # x=len(str(count))
                   for j in str(count):
                       ap.append(j)
                 count=1
-        print(i,chars[i])
         if len(chars)>1:
             if chars[i]==chars[i+1]:
                 ap.append(chars[i])
                 if count>1 and count<10:
                         ap.append(str(count))
                 elif count>=10:
-                    # x=len(str(count))
                     for j in str(count):
                         ap.append(j)
             if chars[i]!=chars[i+1]:
-                # print('q')
-                # print(chars[i+1],chars[i-1])
                 ap.append(chars[i+1])
-                # ap.append('1')
-            print(ap)
             for i in range(len(ap)):
                 chars[i]=ap[i]
             return len(ap)
