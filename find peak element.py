5d14b8991e91056af9f6c694aa32a890c355db36
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-03-03 21:37:45 +0530

    leetcode

---
+++ find peak element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findPeakElement(self, nums: list[int]) -> int:
+        if len(nums)==1:
+            return 0
+        elif len(nums)==2:
+            return nums.index(max(nums))
+        for i in range(1,len(nums)-1):
+            if nums[i]>nums[i-1] and nums[i]>nums[i+1]:
+                return i
+        return nums.index(max(nums))
