3fa02ba96ff42f176314131eddd1b0cd2d89e8c0
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-03-13 17:24:02 +0530

    leetcode

---
+++ ugly number.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/ugly-number/solutions/
+class Solution {
+public:
+    bool isUgly(int n) {
+        while(n>1)
+        {
+            if (n%2==0)
+            {
+                n/=2;
+            }
+            else if(n%3==0)
+            n/=3;
+            else if(n%5==0)
+            {
+                n/=5;
+            }
+            else
+            break;
+        }
+        if(n==1)
+        return true;
+        return false;
+        
+    }
+};
