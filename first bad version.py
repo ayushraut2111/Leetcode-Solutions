80f03a7db66463bc6b7a481ee6f20437bdba0135
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-03-14 16:38:33 +0530

    leetcode

---
+++ first bad version.py
@@ -0,0 +1,20 @@
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        def isBadVersion(x)->bool:
+            pass
+        if n<100:
+            for i in range(1,n+1):
+                if isBadVersion(i)==True:
+                    return i
+        start=1
+        end=n
+        while start<=end:
+            mid=(end+start+1)//2
+            if isBadVersion(mid)==True and isBadVersion(mid-1)==False:
+                return mid
+            elif isBadVersion(mid)==True:
+                end=mid
+            else:
+                start=mid
+            
