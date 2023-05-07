e850df396b07cae648da575a93fe2f5c989a0d91
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-05-07 23:15:58 +0530

    max avg

--- max avg subarray.py
+++ max avg subarray.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/maximum-average-subarray-i
 
 class Solution:
-    def findMaxAverage(self, a):
+    def findMaxAverage(self, a,k):
         ans=-9999999
         s=sum(a[:k])
         ans=max(s,ans)
