0c3416f1cb283773c61529afcf4d950312ce9f31
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-08-11 01:26:13 +0530

    rotate string

---
+++ rotate string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rotateString(self, b: str, a: str) -> bool:
+        temp=a*2
+        if len(a)<len(b):
+            return False
+        for i in range(len(temp)-len(b)+1):
+            if temp[i]==b[0]:
+                k=0
+                count=0
+                for j in range(i,i+len(b)):
+                    if temp[j]==b[k]:
+                        count+=1
+                        k+=1
+                    else:
+                        break
+                if count==len(b):
+                    return True
