bbf29bfaae4d440d742caacd63a8cf163c55acfc
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-08-16 13:28:21 +0530

    intersection point

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-j=None
-for j in range(0,9):
-    pass
-print(j)
+def countNumbersWithUniqueDigits(n):
+        count=0
+        if n==0:
+            return 1
+        if n==1:
+            return 10
+        for i in range(10,(10**n)):
+            a=str(i)
+            p=list(map(int,a))
+            if len(set(p))==len(a):
+                # print(set(p),a)
+                count+=1
+        return count+10
+
+print(countNumbersWithUniqueDigits(8))
